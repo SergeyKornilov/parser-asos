7303c99a61a727f0e23063bf1b8be807bbe145e0
SergeyKornilov <kornilovsergey.box@gmail.com>
2020-10-07 21:36:25 +0300

    new service - CategoryService.getCategoryUrls

---
+++ src/main/java/ru/kornilov/service/parser/CategoryService.java
@@ -0,0 +1,39 @@
+package ru.kornilov.service.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryService {
+
+    public List<String> getCategoryUrls(String url){
+        List<String> urlsPages = new ArrayList<>();
+        double numPages;
+
+        Document page = null;
+        try {
+            page = Jsoup.parse(new URL(url), 10000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        Elements numberOnPage = page.select("p._2JQRAAs");                             //вы просмотрели .. из ..
+
+        String strNumberProd = numberOnPage.text().replaceAll("\\D", "");
+        numPages = Math.ceil(Double.parseDouble(strNumberProd) / 72);
+
+        urlsPages.add(url);
+
+        for (int i = 2; i <= numPages; i++) {
+            urlsPages.add(url + "&page=" + i);                                                                          //Остальные URL получаем и добавляем
+        }
+
+        return urlsPages;
+    }
+}
